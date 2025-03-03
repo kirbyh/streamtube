ade6ada4ace72aee9847bdfa9ec14ae607e4c323
Kirby Heck <kirby.heck@gmail.com>
2025-03-03 11:58:49 -0600

    Added block_reduce function from coriolis package

---
+++ streamtube/block.py
@@ -0,0 +1,179 @@
+"""
+Different blocking functions (replaces skimage.util.shape.view_as_blocks)
+
+Kirby Heck
+2023 October 27
+"""
+
+import numpy as np
+from numpy.lib.stride_tricks import as_strided
+
+
+def block_reduce_centered(image, block_size=3, func=np.sum, pad_kwargs=None, func_kwargs=None):
+    """
+    Modified block_reduce function with centered blocking and padding
+    keyword options. 
+
+    Downsample image by applying function `func` to local blocks.
+
+    This function is useful for max and mean pooling, for example.
+
+    Parameters
+    ----------
+    image : ndarray
+        N-dimensional input image.
+    block_size : array_like or int
+        Array containing down-sampling integer factor along each axis.
+        Default block_size is 3. Must be odd. 
+    func : callable
+        Function object which is used to calculate the return value for each
+        local block. This function must implement an ``axis`` parameter.
+        Primary functions are ``numpy.sum``, ``numpy.min``, ``numpy.max``,
+        ``numpy.mean`` and ``numpy.median``.  See also `func_kwargs`.
+    pad_kwargs : dict
+        Keyword arguments passed into ``np.pad``. 
+    func_kwargs : dict
+        Keyword arguments passed to `func`. Notably useful for passing dtype
+        argument to ``np.mean``. Takes dictionary of inputs, e.g.:
+        ``func_kwargs={'dtype': np.float16})``.
+
+    Returns
+    -------
+    image : ndarray
+        Down-sampled image with same number of dimensions as input image.
+
+    """
+
+    if np.isscalar(block_size):
+        block_size = (block_size,) * image.ndim
+    elif len(block_size) != image.ndim:
+        raise ValueError("`block_size` must be a scalar or have "
+                         "the same length as `image.shape`")
+    
+    center_width = []
+    for nfact in block_size: 
+        if nfact % 2 == 0: 
+            raise ValueError("Centered block_reduce only can take "
+                             "odd numbered block dimensions")
+        center_width.append((nfact // 2, 0)) 
+    
+    if func_kwargs is None:
+        func_kwargs = {}
+    if pad_kwargs is None: 
+        pad_kwargs = {}
+
+    # pad image with "centering" blocks: 
+    image = np.pad(image, pad_width=center_width, mode='edge')
+
+    pad_width = []
+    for i in range(len(block_size)):
+        if block_size[i] < 1:
+            raise ValueError("Down-sampling factors must be >= 1. Use "
+                             "`skimage.transform.resize` to up-sample an "
+                             "image.")
+        if image.shape[i] % block_size[i] != 0:
+            after_width = block_size[i] - (image.shape[i] % block_size[i])
+        else:
+            after_width = 0
+        pad_width.append((0, after_width))
+
+    image = np.pad(image, pad_width=pad_width, 
+                   **pad_kwargs
+                   )
+
+    blocked = view_as_blocks(image, block_size)
+
+    return func(blocked, axis=tuple(range(image.ndim, blocked.ndim)),
+                **func_kwargs)
+
+
+# the following is copied from skimage.util: 
+
+
+def view_as_blocks(arr_in, block_shape):
+    """Block view of the input n-dimensional array (using re-striding).
+
+    Blocks are non-overlapping views of the input array.
+
+    Parameters
+    ----------
+    arr_in : ndarray
+        N-d input array.
+    block_shape : tuple
+        The shape of the block. Each dimension must divide evenly into the
+        corresponding dimensions of `arr_in`.
+
+    Returns
+    -------
+    arr_out : ndarray
+        Block view of the input array.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from skimage.util.shape import view_as_blocks
+    >>> A = np.arange(4*4).reshape(4,4)
+    >>> A
+    array([[ 0,  1,  2,  3],
+           [ 4,  5,  6,  7],
+           [ 8,  9, 10, 11],
+           [12, 13, 14, 15]])
+    >>> B = view_as_blocks(A, block_shape=(2, 2))
+    >>> B[0, 0]
+    array([[0, 1],
+           [4, 5]])
+    >>> B[0, 1]
+    array([[2, 3],
+           [6, 7]])
+    >>> B[1, 0, 1, 1]
+    13
+
+    >>> A = np.arange(4*4*6).reshape(4,4,6)
+    >>> A  # doctest: +NORMALIZE_WHITESPACE
+    array([[[ 0,  1,  2,  3,  4,  5],
+            [ 6,  7,  8,  9, 10, 11],
+            [12, 13, 14, 15, 16, 17],
+            [18, 19, 20, 21, 22, 23]],
+           [[24, 25, 26, 27, 28, 29],
+            [30, 31, 32, 33, 34, 35],
+            [36, 37, 38, 39, 40, 41],
+            [42, 43, 44, 45, 46, 47]],
+           [[48, 49, 50, 51, 52, 53],
+            [54, 55, 56, 57, 58, 59],
+            [60, 61, 62, 63, 64, 65],
+            [66, 67, 68, 69, 70, 71]],
+           [[72, 73, 74, 75, 76, 77],
+            [78, 79, 80, 81, 82, 83],
+            [84, 85, 86, 87, 88, 89],
+            [90, 91, 92, 93, 94, 95]]])
+    >>> B = view_as_blocks(A, block_shape=(1, 2, 2))
+    >>> B.shape
+    (4, 2, 3, 1, 2, 2)
+    >>> B[2:, 0, 2]  # doctest: +NORMALIZE_WHITESPACE
+    array([[[[52, 53],
+             [58, 59]]],
+           [[[76, 77],
+             [82, 83]]]])
+    """
+    if not isinstance(block_shape, tuple):
+        raise TypeError('block needs to be a tuple')
+
+    block_shape = np.array(block_shape)
+    if (block_shape <= 0).any():
+        raise ValueError("'block_shape' elements must be strictly positive")
+
+    if block_shape.size != arr_in.ndim:
+        raise ValueError("'block_shape' must have the same length "
+                         "as 'arr_in.shape'")
+
+    arr_shape = np.array(arr_in.shape)
+    if (arr_shape % block_shape).sum() != 0:
+        raise ValueError("'block_shape' is not compatible with 'arr_in'")
+
+    # -- restride the array to build the block view
+    new_shape = tuple(arr_shape // block_shape) + tuple(block_shape)
+    new_strides = tuple(arr_in.strides * block_shape) + arr_in.strides
+
+    arr_out = as_strided(arr_in, shape=new_shape, strides=new_strides)
+
+    return arr_out
